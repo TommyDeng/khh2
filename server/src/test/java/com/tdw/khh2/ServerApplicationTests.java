4a17d5e85b1eeb96f6953c35b13b8c080f52aa2e
tommydeng <250575979@qq.com>
2021-07-25 12:02:53 +0800

    Initial commit

---
+++ server/src/test/java/com/tdw/khh2/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdw.khh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
